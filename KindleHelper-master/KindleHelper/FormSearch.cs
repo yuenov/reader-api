af5834ae72cb96846dcb4e67e871564feb4d2ef6
wwl16129 <101973823+wwl16129@users.noreply.github.com>
2023-02-24 21:53:29 +0800

    Add files via upload

---
+++ KindleHelper-master/KindleHelper/FormSearch.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using libZhuishu;
+using System.Diagnostics;
+using System.IO;
+namespace KindleHelper
+{
+    public partial class FormSearch : Form
+    {
+        public FormSearch()
+        {
+            InitializeComponent();
+        }
+
+
+        private void btn_search_Click(object sender, EventArgs e)
+        {
+            FormSearchResult form_result = new FormSearchResult();
+            var results = LibZhuiShu.fuzzySearch(textbox_search.Text, 0, 100);
+            if (results == null || results.Length < 1) {
+                MessageBox.Show("没有找到:" + textbox_search.Text);
+                return;
+            }
+            form_result.ShowResult(results);
+        }
+
+        private void textbox_search_TextChanged(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(textbox_search.Text)) return;
+            listbox_autocomplate.Items.Clear();
+            RunAsync(()=> {
+                var words = LibZhuiShu.autoComplate(textbox_search.Text);
+                RunInMainthread(()=> {
+                    if (words != null && words.Length > 0) {
+                        listbox_autocomplate.Items.AddRange(words);
+                        listbox_autocomplate.Visible = true;
+                    } else {
+                        listbox_autocomplate.Visible = false;
+                    }
+                });
+            });
+
+
+        }
+
+        private void listbox_autocomplate_SelectedValueChanged(object sender, EventArgs e)
+        {
+            if (listbox_autocomplate.SelectedItem == null) return;
+
+            textbox_search.Text = listbox_autocomplate.SelectedItem.ToString();
+            listbox_autocomplate.Visible = false;
+            btn_search_Click(sender,e);
+        }
+
+        private void FormSearch_Load(object sender, EventArgs e)
+        {
+            this.Text += " V" + System.Reflection.Assembly.GetExecutingAssembly().GetName().Version.ToString();
+        }
+
+
+        void RunAsync(Action action)
+        {
+            ((Action)(delegate () {
+                action?.Invoke();
+            })).BeginInvoke(null, null);
+        }
+
+        void RunInMainthread(Action action)
+        {
+            this.BeginInvoke((Action)(delegate () {
+                action?.Invoke();
+            }));
+        }
+
+    }
+}
---
+++ KindleHelper-master/README.md
@@ -0,0 +1,12 @@
+# KindleHelper
+## 全网小说搜索&下载.支持Mobi/txt格式.
+### 下载
+* github: https://github.com/qq573011406/KindleHelper/releases
+* 百度网盘: https://pan.baidu.com/s/1dEWLErZ 密码: 2wm8
+### 截图
+![1](https://github.com/qq573011406/KindleHelper/raw/master/KindleHelper/resource/1.png)
+![1](https://github.com/qq573011406/KindleHelper/raw/master/KindleHelper/resource/2.png)
+![1](https://github.com/qq573011406/KindleHelper/raw/master/KindleHelper/resource/3.png)
+![1](https://github.com/qq573011406/KindleHelper/raw/master/KindleHelper/resource/4.jpg)
+![1](https://github.com/qq573011406/KindleHelper/raw/master/KindleHelper/resource/5.jpg)
+![1](https://github.com/qq573011406/KindleHelper/raw/master/KindleHelper/resource/6.jpg)
